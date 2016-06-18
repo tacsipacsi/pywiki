2a87311db4e0d8e609c427bfa579b1db2bb06a08
tacsipacsi <tacsipacsi@users.noreply.github.com>
2016-06-18 17:40:29 +0200

    Create tudakozo.py

---
+++ tudakozo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+This script archives the [[Wikipédia:Tudakozó]] (Reference Desk) on hu.wikipedia.
+"""
+#
+# (C) qcz, 2008-2016
+# (C) Tacsipacsi, 2016
+#
+# Distributed under the GNU General Public License, version 3 or later version.
+#
+from __future__ import absolute_import, unicode_literals
+
+import re
+import datetime
+from datetime import date
+import pywikibot
+
+def main(*args):
+    local_args = pywikibot.handle_args(args)
+    
+    site = pywikibot.Site('hu', 'wikipedia')
+    
+    ma = (date.today() - datetime.timedelta(days=1)).isoformat()
+    maiArch = pywikibot.Page(site, 'Tudakozó/Archívum/' + ma, 4)
+    if maiArch.exists():
+        pywikibot.output('Ma már történt archiválás...')
+    else:
+        tudMost = pywikibot.Page(site, 'Tudakozó', 4)
+        tudMost = tudMost.move(maiArch.title(), reason='Bot: Tudakozó napi archiválása', movetalkpage=False)
+        regexp = r'\{\{/Fejrész\}\}'
+        if re.search(regexp, tudMost.text) != None:
+            tudMost.text = re.sub(regexp, '<noinclude>{{Tudakozó-keretes}}</noinclude>', tudMost.text)
+            tudMost.save(summary='Bot: fejrész cseréje az archívumsablonra', minor=False)
+        
+        ujMost = pywikibot.Page(site, 'Tudakozó', 4)
+        ujMost.text = '{{/Fejrész}}'
+        ujMost.save(summary='Bot: új, üres oldal elhelyezése', minor=True)
+
+if __name__ == '__main__':
+    main()
